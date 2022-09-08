c30f91935d57ff51557b333773718307bf6c319a
jamesmugi <jameskaranja109@gmail.com>
2022-09-08 13:43:57 +0300

    Updated code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
  *
- * Return :Always 0 (Success)
+ * Return: Always 0 (Success)
  */
-#display output and return 0
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
