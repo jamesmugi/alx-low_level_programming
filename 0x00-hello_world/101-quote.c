cb9eeefb0bafb3e1ea7824476ba3fc88716c8dc7
jamesmugi <jameskaranja109@gmail.com>
2022-09-08 14:17:54 +0300

    Rectify error in doc

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 
 /**
- * Return
+ * main - Entry point
  *
  * Return : Always 0 (Success)
  */
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n");
+	printf("and that piece of art is useful\"
+		       	- Dora Korpar, 2015-10-19 \n");
 	return (1);
 }
